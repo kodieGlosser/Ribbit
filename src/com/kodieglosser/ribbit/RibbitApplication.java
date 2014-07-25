1943b4f41b9883c4522794f4044752a1d69872bb
kodieGlosser <kodieglosser@gmail.com>
2014-07-24 23:31:01 -0400

    second commit

---
+++ src/com/kodieglosser/ribbit/RibbitApplication.java
@@ -0,0 +1,15 @@
+package com.kodieglosser.ribbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class RibbitApplication extends Application {
+	
+	@Override
+	public void onCreate() {
+		  super.onCreate();
+		  Parse.initialize(this, "xYqMPxRjHM5YTRKj2CP5mXVcLEOKeJ364E3lLaJk", "33vtslH4na9cgczcDOsZE932qHtLCj4MbXSHp3Y3");
+		}
+}
