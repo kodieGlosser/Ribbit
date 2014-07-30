b24aa7bbcb4828f3f46e29075258eed93b2d384c
kodieGlosser <kodieglosser@gmail.com>
2014-07-29 21:14:41 -0400

    Relating Users in Parse.com

--- src/com/kodieglosser/ribbit/MainActivity.java
+++ src/com/kodieglosser/ribbit/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 import com.parse.ParseUser;
 
@@ -35,8 +36,9 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_main);
-        
+   
         ParseUser currentUser = ParseUser.getCurrentUser();
         if (currentUser == null) {	
 	        navigateToLogin();
